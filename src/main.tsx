a2973ca534c0f22491989ac777fc0e712b0298cf
rssXX <31318078+rssXX@users.noreply.github.com>
2024-12-12 11:07:27 +0500

    install nuqs

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { NuqsAdapter } from 'nuqs/adapters/react'
 import {MainPage} from './pages'
 import './utils/styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MainPage />
+      <NuqsAdapter>
+          <MainPage />
+      </NuqsAdapter>
   </StrictMode>,
 )
