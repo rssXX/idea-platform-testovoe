7be75ea0dfd35af64961bd35a5edde4d79509c61
rssXX <31318078+rssXX@users.noreply.github.com>
2024-12-13 10:38:08 +0500

    delete nuqs

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { NuqsAdapter } from 'nuqs/adapters/react'
 import {MainPage} from './pages'
 import './utils/styles/global.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-      <NuqsAdapter>
-          <MainPage />
-      </NuqsAdapter>
+      <MainPage />
   </StrictMode>,
 )
